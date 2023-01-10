0d0341feb1892315945de0aa5b06d0fd8ff528cb
nemanja899 <zakizaja@hotmail.com>
2023-01-10 14:25:26 +0100

    first commit

---
+++ frontend/src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CalltoActionSection = ()=>{
+    return(
+        <div className="subscibe-section bg-with-black">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <div className="subscibe-head">
+                            <h2></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default CalltoActionSection;
