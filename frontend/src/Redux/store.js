0a02f0e7921079bfa42963a1b5134303f57a81b5
nemanja899 <zakizaja@hotmail.com>
2023-02-21 20:38:19 +0100

    adding login redux

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -6,22 +6,29 @@ import {
   productListReducer,
 } from "./Reducers/ProductReducers";
 import { cartReducer } from "./Reducers/CartReducers";
+import { userLoginReducer } from "./Reducers/UserReducer";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailReducer,
   cart: cartReducer,
+  userLogin:userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItem")
   ? JSON.parse(localStorage.getItem("cartItem"))
   : [];
 
+//login
+  const userInfoFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const initialState = {
     cart: {
         cartItems:cartItemsFromLocalStorage
-    }
+    },
+    userLogin:{userInfo: userInfoFromLocalStorage },
 };
 
 const middleWare = [thunk];
